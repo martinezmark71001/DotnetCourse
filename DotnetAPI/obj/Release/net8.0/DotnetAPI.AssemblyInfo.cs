c1cfe559e4a2e5c6992f2e62fe25f97c6a9ec5fd
martinezmark71001 <martinezmark71001@gmail.com>
2025-04-25 12:27:55 +0800

    Solution update

--- DotnetAPI/obj/Release/net8.0/DotnetAPI.AssemblyInfo.cs
+++ DotnetAPI/obj/Release/net8.0/DotnetAPI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69a8a87deac69204e58c0d52576124917142f72a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
